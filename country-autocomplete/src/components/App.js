39f45d70243c8df369396a0179a92ce40c2c3e0c
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-22 16:53:22 +0200

    # Create state for search string and pass to SearchBar component

--- country-autocomplete/src/components/App.js
+++ country-autocomplete/src/components/App.js
@@ -4,18 +4,24 @@ import SearchBar from './SearchBar';
 import { countries } from '../countries';
 
 function App() {
-  const [countriesArr, SetCountriesArr] = useState(countries);
+  const [countriesArr, setCountriesArr] = useState(countries);
+  const [searchStr, setSearchStr] = useState('');
+
+  const changeSearchStr = (str) => {
+    setSearchStr(str);
+  };
 
   const filterCountries = (str) => {
     str = str.toLowerCase();
     const filterArr = countriesArr.filter((country) => country.label.toLowerCase().includes(str));
     return filterArr;
   };
+
   return (
     <div>
       <h1>Country-Autocomplete</h1>
-      <SearchBar />
-      <DropDown countries={filterCountries('')} />
+      <SearchBar onStringChange={changeSearchStr} />
+      <DropDown countries={filterCountries(searchStr)} />
     </div>
   );
 }
---
+++ country-autocomplete/src/components/SearchBar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SearchBar() {
+  return <input type='text' placeholder='country name..' />;
+}
+
+export default SearchBar;
