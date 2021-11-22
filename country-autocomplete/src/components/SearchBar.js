916e751006685723608f7ccc7eef2ade77c50ab3
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-11-22 19:01:17 +0200

    # Add clear button

--- country-autocomplete/src/components/SearchBar.js
+++ country-autocomplete/src/components/SearchBar.js
@@ -3,16 +3,25 @@ import React, { useRef } from 'react';
 function SearchBar(props) {
   const searchInput = useRef('');
   return (
-    <input
-      type='text'
-      placeholder='country name..'
-      ref={searchInput}
-      value={props.pickedCountry}
-      onChange={() => {
-        props.onStringChange(searchInput.current.value ? searchInput.current.value : '');
-      }}
-      onClick={props.onSearchBarClick}
-    />
+    <div>
+      <input
+        type='text'
+        placeholder='country name..'
+        ref={searchInput}
+        value={props.pickedCountry}
+        onChange={() => {
+          props.onStringChange(searchInput.current.value ? searchInput.current.value : '');
+        }}
+        onClick={props.onSearchBarClick}
+      />
+      <button
+        onClick={() => {
+          props.onStringChange('');
+        }}
+      >
+        ❌
+      </button>
+    </div>
   );
 }
 
